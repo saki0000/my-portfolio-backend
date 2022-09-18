410a33d12edbf0c930e98352480c5a551516b486
saki000 <hangchengtengqi@gmail.com>
2022-09-18 19:53:32 +0900

    shuusei

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,4 +2,9 @@
 # exit on error
 set -o errexit
 
-bundle install && yarn install&&bundle exec rake assets:precompile&&bundle exec rake assets:clean&&bundle exec rails webpacker:compile&&bundle exec rake db:migrate
+bundle install
+yarn install
+bundle exec rake assets:precompile
+bundle exec rake assets:clean
+bundle exec rails webpacker:compile
+bundle exec rake db:migrate
