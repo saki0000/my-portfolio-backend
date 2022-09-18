d13e1bb1bfe0bd25614b8b17ffdc896551388349
saki000 <hangchengtengqi@gmail.com>
2022-09-18 19:58:27 +0900

    shuusei

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,7 +4,6 @@ set -o errexit
 
 bundle install
 yarn install
-bundle exec rake assets:precompile
-bundle exec rake assets:clean
+# bundle exec rake assets:clean
 bundle exec rails webpacker:compile
 bundle exec rake db:migrate
