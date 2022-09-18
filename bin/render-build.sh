04ce78592a7de72a5445dfa41febef9c84cd082a
saki000 <hangchengtengqi@gmail.com>
2022-09-18 19:11:29 +0900

    修正

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,5 +1,6 @@
 set -o errexit
 
-bundle install &&  bundle lock --add-platform x86_64-linux
+bundle install
+bundle lock --add-platform x86_64-linux
 bundle exec rake assets:clean
 bundle exec rake db:migrate
