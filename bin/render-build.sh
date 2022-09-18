beb60094e3451a765d87f923e09980f628f2bc68
saki000 <hangchengtengqi@gmail.com>
2022-09-18 19:03:18 +0900

    render-buildの修正

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,5 +1,5 @@
 set -o errexit
 
-bundle install && yarn install && bundle exec rake assets:precompile && bundle exec rake assets:clean && bundle exec rails webpacker:compile && bundle exec rake db:migratebundle exec rake assets:precompile
+bundle install && yarn install && bundle exec rake assets:precompile && bundle exec rake assets:clean && bundle exec rails webpacker:compile && bundle exec rake db:migratebundle exec rake assets:precompile && bundle lock --add-platform x86_64-linux
 bundle exec rake assets:clean
 bundle exec rake db:migrate
